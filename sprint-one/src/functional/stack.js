f768fb76267af6a70a0034dd74c5a421bd2c0b99
Jazz <ripley36706@gmail.com>
2015-10-07 12:38:28 -0700

    Recreating Stack Example

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,29 @@
 var Stack = function(){
-  var someInstance = {};
+    var someInstance = {};
+    var index = 0;
 
   // Use an object with numeric keys to store values
-  var storage = {};
+    var storage = {};
 
-  // Implement the methods below
-  someInstance.push = function(value){
-  };
+    // Implement the methods below
+    someInstance.push = function(value){
+        storage[index] = value;
+        index++;
+    };
 
-  someInstance.pop = function(){
-  };
+    someInstance.pop = function(){
+        if(index > 0){
+            delete storage[index - 1];
+            
+            index--;           
+        }
+        return storage[index -1];
+    };
 
-  someInstance.size = function(){
-  };
+    someInstance.size = function(){
+        
+        return index;
+    };
 
-  return someInstance;
+    return someInstance;
 };
