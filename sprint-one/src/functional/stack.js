a6e511eacb1392d7fa9777dd87c3d2a3d300ff4f
existenzial <ripley36706@gmail.com>
2015-10-07 16:38:05 -0700

    Completed Functional Stack/Class

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function(){
   var someInstance = {};
-
+  var index = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    someInstance[index] = value;
+    index++;
   };
 
   someInstance.pop = function(){
+    if ( index > 0){
+      var lastVal = someInstance[index-1];
+      delete someInstance[index - 1];
+      index--;
+    }
+    return lastVal;
   };
 
   someInstance.size = function(){
+    return index;
   };
 
   return someInstance;
