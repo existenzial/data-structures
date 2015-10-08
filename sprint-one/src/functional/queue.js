ee5622ffbd80fc8cb47f4e45efa9a7c78f576c5c
Jazz Lyles <existenzial@users.noreply.github.com>
2015-10-07 20:03:26 -0700

    Update queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,31 @@
 var Queue = function(){
   var someInstance = {};
-
+  var firstIndex = 0;
+  var lastIndex = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    someInstance[lastIndex] = value;
+    lastIndex++;
   };
 
   someInstance.dequeue = function(){
+    //front of line
+    var firstInLine = someInstance[firstIndex];
+    delete someInstance[firstIndex];
+    firstIndex++;
+    return firstInLine;
   };
 
   someInstance.size = function(){
+    return Object.keys(someInstance).length - 3;
+   // var keyLength = Object.keys(someInstance);
+   // return keyLength.length;
   };
 
   return someInstance;
+
 };
