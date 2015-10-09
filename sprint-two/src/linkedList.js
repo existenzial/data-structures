313eeb471e12fef69e9c4540439867055ec7fc59
existenzial <ripley36706@gmail.com>
2015-10-09 16:42:20 -0700

    linkedList in Progress

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,28 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    
+
+    list.tail = new Node(value);
+    
+    if(list.head === null) {
+      list.head = list.tail;
+      list.head.next = list.tail;
+    }
+
   };
 
   list.removeHead = function(){
+
+    if(list.head === null) {
+      console.log("list is empty");
+    }
+    var originalHead = list.head;
+    var newHead = list.head.next;
+
+    list.head = null;
+    list.head = newHead; 
+
   };
 
   list.contains = function(target){
@@ -23,7 +42,17 @@ var Node = function(value){
 
   return node;
 };
+var jvList = new LinkedList();
+jvList.addToTail(5);
+jvList.addToTail(4);
+jvList.addToTail(3);
+jvList.addToTail(2);
+jvList.addToTail(1);
+jvList.removeHead();
+console.log(jvList.list);
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ Linear!
  */
