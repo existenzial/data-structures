2c2be9e8bf2ec7c2bc9023f636b3c5c618f4e8e6
existenzial <ripley36706@gmail.com>
2015-10-09 15:23:58 -0700

    Completed Data Structures - Sprint 1

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.queuedIndex = 0;
+  this.lastIndex = 0;
+
 };
+
+Queue.prototype.enqueue = function (value){
+    this.storage[this.queuedIndex + this.lastIndex] = value;
+    this.queuedIndex++;
+};
+
+Queue.prototype.dequeue = function (){
+  if (this.queuedIndex > 0){
+    var firstInLine = this.storage[this.lastIndex];
+    delete this.storage[this.lastIndex];
+    this.queuedIndex--;
+    this.lastIndex++;
+  }
+  return firstInLine;
+};
+
+Queue.prototype.size = function (){
+  return this.queuedIndex;
+};
+
+var queueInstance = new Queue();
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,48 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.index = 0;
 };
 
+Stack.prototype.push = function (value){
+    this.storage[this.index] = value;
+    this.index++;
+  };
+
+Stack.prototype.pop = function (){
+    if (this.index > 0){
+      var lastVal = this.storage[this.index - 1];
+      delete this.storage[this.index - 1];
+      this.index--;
+    }
+    return lastVal;
+  };
+
+Stack.prototype.size = function (){
+    return this.index;
+  };
+
+var stackInstance = new Stack();
+
+/*
+Stack.prototype = {
+
+  push: function (value){
+    this.storage[this.index] = value;
+    this.index++;
+  },
+   
+  pop: function (){
+    if (this.index > 0){
+      var lastVal = this.storage[this.index - 1];
+      delete this.storage[this.index - 1];
+      this.index--;
+    }
+    return lastVal;
+  },
+  
+  size: function (){
+    return this.index;
+  }
+};
+*/
 
