7ae0b8323eaa45678b1d9f4ebd71907b88496bce
existenzial <ripley36706@gmail.com>
2015-10-09 19:11:14 -0700

    Added Pseudo-code Based Off of Diagram

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,32 +3,33 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
-    
 
-    list.tail = new Node(value);
-    
-    if(list.head === null) {
+  list.addToTail = function(value){
+    //if there is no value for the Head(null) create it, then set the value to the tail
+    //once the head exists, the next node will get added to the tail
+
+    if(list.head === null){
+      list.tail = new Node(value);
       list.head = list.tail;
-      list.head.next = list.tail;
+    } else {
+      list.head.next = 
     }
+    
+    
 
   };
 
   list.removeHead = function(){
-
-    if(list.head === null) {
-      console.log("list is empty");
-    }
-    var originalHead = list.head;
-    var newHead = list.head.next;
-
-    list.head = null;
-    list.head = newHead; 
+    //will remove the first node
+    //then will make the nextNode the value of the Head to give us a new Head
+    //if one Node left, the Head is = Tail, and if it is removed return an empty node object
+    
 
   };
 
   list.contains = function(target){
+    //if target node or value exists in the list, return it
+    //else add the target node to the tail
   };
 
   return list;
@@ -39,17 +40,18 @@ var Node = function(value){
 
   node.value = value;
   node.next = null;
+  //node.next refers to what comes after the target node that we searched the list for with .contains
 
   return node;
 };
 var jvList = new LinkedList();
-jvList.addToTail(5);
-jvList.addToTail(4);
-jvList.addToTail(3);
-jvList.addToTail(2);
 jvList.addToTail(1);
+jvList.addToTail(2);
+jvList.addToTail(3);
 jvList.removeHead();
-console.log(jvList.list);
+console.log('list',jvList);
+console.log('tail ',jvList.tail);
+console.log('head ',jvList.head);
 
 
 /*
