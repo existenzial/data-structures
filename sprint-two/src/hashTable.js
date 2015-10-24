8eab72705b14c6c5e68a7dbc58d6c8b67d46ecd7
Jazz Lyles <existenzial@users.noreply.github.com>
2015-10-24 13:50:26 -0700

    Update hashTable.js
    Hash table complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,16 +4,60 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+    var i = getIndexBelowMaxForKey(k, this._limit);
+    var bucket = this._storage.get(i);
+    //assign the value of the bucket
+    var found = false;
+    //assign a starting value of false for whether the tuple was found before checking
+    if(!bucket){
+    //if bucket doesn't exist
+        bucket = [];
+        //create it
+        this._storage.set(i, bucket);
+        //then store the value of that bucket
+        
+    }
+    this._storage.get(i).push([k,v]);
+    //then we push the key/val pair into the bucket
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-
+    var i = getIndexBelowMaxForKey(k, this._limit);
+    var bucket = this._storage.get(i);
+    
+    if (!bucket){
+        return null;
+        //return null if the bucket isn't found
+    }
+    for (var t = 0; t < bucket.length; t++){
+        var tuple = bucket[t];
+        //assign the value of a tuple to each index position of the bucket
+        if(tuple[0] === k){
+        //if key is found
+            return tuple[1];
+            //return its associated value
+        }
+    }
 };
 
 HashTable.prototype.remove = function(k){
-
+    var i = getIndexBelowMaxForKey(k, this._limit);
+    var bucket = this._storage.get(i);
+    
+    if (!bucket){
+        return null;
+        //return null if the bucket isn't found
+    }
+    for (var t = 0; t < bucket.length; t++){
+        var tuple = bucket[t];
+        //assign the value of a tuple to each index position of the bucket
+        if(tuple[0] === k){
+        //if key is found
+            tuple[1] = null;
+            return;
+            //return its associated value
+        }
+    }
 };
 
 
